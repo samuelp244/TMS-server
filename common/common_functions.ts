092c7eb98335d6219719847f59d64714730d5884
samuelp244 <samuelp244@gmail.com>
2022-11-30 19:37:20 +0530

    using multipart to handle images

--- common/common_functions.ts
+++ common/common_functions.ts
@@ -1,7 +1,26 @@
-
-export const getDateTime = () =>{
-    const dateTime = new Date();
-    const date = dateTime.getFullYear()+"-"+(dateTime.getMonth()+1)+"-"+dateTime.getDate();
-    const time = dateTime.getHours()+":"+dateTime.getMinutes()+":"+dateTime.getSeconds();
-    return date+" "+time; 
-}
+export const getDateTime = () => {
+  const dateTime = new Date();
+  const date =
+    dateTime.getFullYear() +
+    "-" +
+    (dateTime.getMonth() + 1) +
+    "-" +
+    dateTime.getDate();
+  const time =
+    dateTime.getHours() +
+    ":" +
+    dateTime.getMinutes() +
+    ":" +
+    dateTime.getSeconds();
+  return date + " " + time;
+};
+export const generateKey = () => {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < 32; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
