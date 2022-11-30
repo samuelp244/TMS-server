7aea7e54906a1624e02847dc607a4b6545fc1b9e
samuelp244 <samuelp244@gmail.com>
2022-12-01 01:05:00 +0530

    .

--- Requests/s3Handler.ts
+++ Requests/s3Handler.ts
@@ -5,23 +5,27 @@ import images from "../models/images.model";
 import { generateKey, getDateTime } from "../common/common_functions";
 import fs from "fs";
 import util from "util";
-import multiparty from "multiparty"
+import multiparty from "multiparty";
 
 const unlinkFile = util.promisify(fs.unlink);
 
 export const uploadHandler = async (req: Request, res: Response) => {
   const form = new multiparty.Form();
-  form.parse(req, async(_err, _fields, files) => {
+  form.parse(req, async (_err, _fields, files) => {
     // console.log(files)
     // console.log(_fields)
-    const file = new Uint8Array(Buffer.from(_fields.image))
-    console.log(file)
-    fs.writeFile('hello.jpg',file)
+    const file = new Uint8Array(Buffer.from(_fields.image));
+    console.log(file);
+    fs.writeFile("hello.jpg", file, callback);
+    const callback = (err) => {
+      if (err) throw err;
+      console.log("It's saved!");
+    };
     const currDateTime = getDateTime();
     const newKey = generateKey();
     try {
       if (_fields.username) {
-        const result = await uploadFile(file,newKey);
+        const result = await uploadFile(file, newKey);
         await unlinkFile(file?.path);
         await images.create({
           username: _fields.username[0],
@@ -37,10 +41,7 @@ export const uploadHandler = async (req: Request, res: Response) => {
       console.log(e);
       res.json({ message: "upload failed" });
     }
-
-  })
-
- 
+  });
 };
 
 export const downloadHandler = async (req: Request, res: Response) => {
